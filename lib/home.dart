becf85c02d0201eaa02f24c5863166c188c02dc6
Abu Niaz <niaz35-507@diu.edu.bd>
2022-06-22 11:34:15 +0600

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -8,11 +8,20 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  double _height = 55;
+  double _width = 55;
+  Color _color = Colors.green;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: AnimatedContainer(duration: Duration()),
+      body: AnimatedContainer(
+        duration: const Duration(seconds: 1),
+        height: _height,
+        width: _width,
+        decoration: BoxDecoration(
+            color: _color, borderRadius: BorderRadius.circular(30)),
+      ),
     );
   }
 }
