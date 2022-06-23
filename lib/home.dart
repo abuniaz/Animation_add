54953b3821a87e2cc754883532c34adacf6ad9a3
Abu Niaz <niaz35-507@diu.edu.bd>
2022-06-23 19:02:20 +0600

    yes

--- lib/home.dart
+++ lib/home.dart
@@ -16,23 +16,25 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: AnimatedContainer(
-        duration: const Duration(seconds: 1),
-        height: _height,
-        width: _width,
-        decoration: BoxDecoration(
-          color: _color,
-          borderRadius: BorderRadius.circular(30),
-        ),
-        curve: Curves.fastOutSlowIn,
-        child: GestureDetector(
-          onTap: () {
-            setState(() {
-              selected = !selected;
-              Colors.teal;
-              _width = 200;
-            });
-          },
+      body: Center(
+        child: AnimatedContainer(
+          duration: const Duration(seconds: 1),
+          height: _height,
+          width: _width,
+          decoration: BoxDecoration(
+            color: _color,
+            borderRadius: BorderRadius.circular(30),
+          ),
+          curve: Curves.fastOutSlowIn,
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                selected = !selected;
+                Colors.teal;
+                _width = 200;
+              });
+            },
+          ),
         ),
       ),
     );
