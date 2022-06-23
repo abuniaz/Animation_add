80a16ce97e7307521bc4b6ffc457b37df4a0bdea
Abu Niaz <niaz35-507@diu.edu.bd>
2022-06-23 19:35:27 +0600

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -8,33 +8,38 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-  final double _height = 55;
+  double _height = 55;
   double _width = 55;
-  final Color _color = Colors.green;
+  Color _color = Colors.green;
   bool selected = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: AnimatedContainer(
-          duration: const Duration(seconds: 1),
-          height: _height,
-          width: _width,
-          decoration: BoxDecoration(
-            color: _color,
-            borderRadius: BorderRadius.circular(30),
-          ),
-          curve: Curves.fastOutSlowIn,
-          child: GestureDetector(
-            onTap: () {
-              setState(() {
-                selected = !selected;
-                Colors.teal;
-                _width = 200;
-              });
-            },
-          ),
+        child: Row(
+          children: [
+            AnimatedContainer(
+              duration: const Duration(seconds: 1),
+              height: _height,
+              width: _width,
+              decoration: BoxDecoration(
+                color: _color,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              curve: Curves.fastOutSlowIn,
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selected = !selected;
+                    _color = Colors.teal;
+                    _width = 200;
+                    _height = 200;
+                  });
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
